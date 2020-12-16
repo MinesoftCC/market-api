27fc46fac10c945911747a59a6b948be0916d37d
STBoyden <stboyden@stboyden.xyz>
2020-12-16 00:11:32 +0000

    Add default settings for MarketData

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,7 @@
 use chrono::{DateTime, Utc};
+use std::{borrow::Cow, default::Default, fs};
+
+type Str = Cow<'static, str>;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MarketData {
@@ -13,10 +16,25 @@ impl MarketData {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Item {
-    pub item_id: String,
-    pub display_name: String,
+    pub item_id: Str,
+    pub item_image_url: Str,
+    pub display_name: Str,
     pub quantity: u32,
     pub price: u32,
     pub poster_id: u16,
     pub time_posted: DateTime<Utc>,
+}
+
+impl Default for Item {
+    fn default() -> Self {
+        Self {
+            item_id: "null".into(),
+            item_image_url: "null".into(),
+            display_name: "null".into(),
+            quantity: 0,
+            price: 0,
+            poster_id: 1,
+            time_posted: Utc::now(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,92 @@ mod routes;
 
 #[macro_use]
 extern crate rocket;
-
 #[macro_use]
 extern crate lazy_static;
-
 #[macro_use]
 extern crate serde;
 
 use crate::data::*;
 use dotenv::dotenv;
-use std::{borrow::Cow, fs::File, io::prelude::*, path::Path, sync::Mutex};
+use rocket::Request;
+use rocket_contrib::json::Json;
+use std::{
+    borrow::Cow,
+    fs::{self, OpenOptions},
+    io::prelude::*,
+    sync::Mutex,
+};
 
 type StrRet = Cow<'static, str>;
 
 lazy_static! {
-    static ref MARKET_DATA: Mutex<MarketData> = Mutex::from(MarketData { items: Vec::new() });
+    static ref MARKET_DATA: Mutex<MarketData> = Mutex::from(MarketData {
+        items: vec![Item::default()]
+    });
+}
+
+#[derive(Deserialize, Serialize)]
+struct ErrorResponse {
+    code: u32,
+    message: String,
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> Json<ErrorResponse> {
+    Json(ErrorResponse {
+        code: 404,
+        message: format!("'{}' not found", req.uri()),
+    })
+}
+
+#[catch(500)]
+fn internal_server_error(req: &Request) -> Json<ErrorResponse> {
+    Json(ErrorResponse {
+        code: 500,
+        message: format!(
+            "Welp, it looks like either the server broke on it's own or you somehow managed to break the server by \
+             yourself. You'll be hearing from our lawyers.<br /><br />Just kidding, we can't afford lawyers.<br />The \
+             following caused an internal server error: '{:?}'.",
+            req
+        ),
+    })
 }
 
 fn check_file() -> Result<(), StrRet> {
-    if !Path::new("data/data.json").exists() {
-        if let Err(e) = File::create("data/data.json") {
-            return Err(format!("Could not create data.json: {}", e).into());
-        }
+    let mut file = match OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open("data/data.json")
+    {
+        Ok(f) => f,
+        Err(e) => return Err(format!("Could not find/create data.json: {}", e).into()),
+    };
 
-        let mut file = File::create("data/data.json").unwrap();
-        let data = serde_json::to_string(&*MARKET_DATA.lock().unwrap()).unwrap();
-        file.write_all(data.as_bytes()).unwrap();
-    }
-
-    let mut file = File::open("data/data.json").unwrap();
     let mut buffer = vec![];
     file.read_to_end(&mut buffer).unwrap();
-    let data = String::from_utf8(buffer.to_vec()).unwrap();
+    let mut data = String::from_utf8(buffer.to_vec()).unwrap();
+    let mut market_data = MARKET_DATA.lock().unwrap();
 
-    *MARKET_DATA.lock().unwrap() = serde_json::from_str(&data).unwrap();
+    if data.is_empty() {
+        data = serde_json::to_string_pretty(&*market_data).unwrap();
+        buffer = data.as_bytes().to_vec();
+        file.write_all(&mut buffer).unwrap();
+    }
+
+    file.read_to_end(&mut buffer).unwrap();
+    *market_data = match serde_json::from_str(&data) {
+        Ok(md) => md,
+        Err(_) => {
+            data = serde_json::to_string_pretty(&*market_data).unwrap();
+            buffer = data.as_bytes().to_vec();
+
+            fs::write("data/data.json", &mut buffer).unwrap();
+            file.read_to_end(&mut buffer).unwrap();
+
+            serde_json::from_str(&data).unwrap()
+        },
+    };
 
     Ok(())
 }
@@ -53,5 +105,6 @@ fn main() {
 
     rocket::ignite()
         .mount("/", routes![routes::products::all_products])
+        .register(catchers![not_found, internal_server_error])
         .launch();
 }
