35540a8f7a335409f98a374ceb0fbfbc5728a315
STBoyden <stboyden@stboyden.xyz>
2021-01-18 19:17:35 +0000

    Update purchasing function

--- src/routes/transactions.rs
+++ src/routes/transactions.rs
@@ -1,4 +1,4 @@
-use crate::{bank::data::*, MARKET_DATA};
+use crate::{bank::data::*, data::ItemRatio, MARKET_DATA};
 use rocket_contrib::json::Json;
 
 #[derive(Deserialize, Serialize)]
@@ -7,15 +7,21 @@ pub enum PurchaseResult {
     Fail(String),
 }
 
-#[get("/buy/<market_id>/<user_id>/<account_name>/<password>/<quantity>")]
+#[post(
+    "/buy/<market_id>/<user_id>/<account_name>/<diamond_amount>",
+    format = "application/json",
+    data = "<password>"
+)]
 pub fn purchase(
     market_id: String,
     user_id: usize,
     account_name: String,
-    password: String,
-    quantity: u32,
+    diamond_amount: u32,
+    password: Json<String>,
 ) -> Json<PurchaseResult> {
     let md = MARKET_DATA.lock().unwrap();
+
+    let password = password.to_string();
 
     // get info about user
     let mut bank = Bank::connect();
@@ -47,6 +53,40 @@ pub fn purchase(
                 "Could not find seller user with ID '{}'",
                 item.poster_id
             ))),
+    };
+
+    let sixteen_stack_ids = [
+        "minecraft:snowball",
+        "minecraft:bucket",
+        "minecraft:egg",
+        "minecraft:oak_sign",
+        "minecraft:spruce_sign",
+        "minecraft:birch_sign",
+        "minecraft:jungle_sign",
+        "minecraft:acacia_sign",
+        "minecraft:dark_oak_sign",
+        "minecraft:crimson_sign",
+        "minecraft:warped_sign",
+        "minecraft:ender_pearl",
+        "minecraft:honey_bottle",
+    ];
+
+    let quantity = match item.item_ratio {
+        ItemRatio::Pair => diamond_amount * 2,
+        ItemRatio::HalfStack =>
+            if sixteen_stack_ids.contains(&&*item.item_id) {
+                diamond_amount * 8
+            } else {
+                diamond_amount * 32
+            },
+        ItemRatio::Stack =>
+            if sixteen_stack_ids.contains(&&*item.item_id) {
+                diamond_amount * 16
+            } else {
+                diamond_amount * 64
+            },
+        ItemRatio::Custom(amt) => diamond_amount * amt,
+        _ => diamond_amount,
     };
 
     if quantity == 0 {
@@ -107,7 +147,9 @@ pub fn purchase(
     // ----
 
     Json(PurchaseResult::Success(format!(
-        "Bought {} {}(s) for {} EO each!",
-        quantity, item.display_name, item.price
+        "Bought {} {}(s) for {} diamonds!",
+        quantity,
+        item.display_name,
+        (quantity * item.price)
     )))
 }
